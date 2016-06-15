38c3b9dad8cdccd76fa5e0a9d354408adaf92913
JingTingZeng <kelly20829tw@gmail.com>
2016-06-15 17:52:30 +0800

    map5

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 //google map
-function initialize() {
+/*function initialize() {
 	var mapOptions = {
 	   	zoom: 5,//縮放層級
 	    center: new google.maps.LatLng(120.22, 22.44),//經緯度
@@ -8,7 +8,7 @@ function initialize() {
 
 	var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);*/
 //延後載入地圖
 /*function loadScript() {
 	var script = document.createElement('script');
