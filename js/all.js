009ed0c78cb01c62d9355847d67d945e6a81ac80
JingTingZeng <kelly20829tw@gmail.com>
2016-06-16 19:48:00 +0800

    marker

--- js/all.js
+++ js/all.js
@@ -1,17 +1,18 @@
 //google map
 var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: 22.7175372, lng: 120.30318710000006},
-		    zoom: 12
-		  });
-		  var image='img/toilet.png';
-		  var beachMaker=new google.maps.Maker({
-		  	position:{lat: 22.7241687,lng:1 20.2766451},
-		  	map:map,
-		  	icon:image
-		  });
-		}
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 22.7175372, lng: 120.30318710000006},
+			zoom: 12
+		});
+	}
+	var image='img/toilet.png';
+	var myLatLng= new google.maps.LatLng(22.7241687, 120.2766451);
+	var beachMarker=new google.maps.Marker({
+		position:myLatLng,
+		map:map,
+		icon:image
+	});
 function codeAddress(){
 	var add = "高雄市楠梓區";
 	var geocoder = new google.maps.Geocoder();
