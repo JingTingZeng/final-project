6c1237620e01364bdd742fa8b3c5082080a7b620
JingTingZeng <kelly20829tw@gmail.com>
2016-06-18 00:06:53 +0800

    search_map

--- js/all.js
+++ js/all.js
@@ -1,11 +1,64 @@
 $(document).ready(init);
 function init(){
-	//初始位置
+	//初始位置(列出楠梓區所有的點)
+	var data1;
+    $.when(
+        $.ajax({
+        	type:'GET',
+        	url:'http://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AH&CaseNo2=5&FileType=2&Lang=C&FolderType=U',
+        	async: false,
+        	dataType: 'json', 
+        	success:function(data){
+        		data1=data; 
+      		}
+        })
+        ).then(function(){
+	        var first = true;
+	        //對話框
+	        var infowindow = new google.maps.InfoWindow();
+	        var map;
+	        if(select1 == "null"){
+	        	for(i=0;i<data1.length;i++){
+		            if (first == true) {//第一次執行迴圈
+	                    /*以哪個緯經度中心來產生地圖*/
+	                    var latlng = new google.maps.LatLng(22.44, 120.20);
+	                    var myOptions = {
+	                        zoom: 11,
+	                        center: latlng,
+	                        mapTypeId: google.maps.MapTypeId.ROADMAP
+	                    };
+	                    /*產生地圖*/
+	                    map = new google.maps.Map($("#map")[0], myOptions);
 
+	                    first = false;
+	                } //End if (first == true) 
+		            var str;
+		            var area=data1[i].address;
+		            var type=data1[i].name;
+		                if(area.match("楠梓區")){
+			                //建立緯經度座標
+	                    	var myLatlng = new google.maps.LatLng(data1[i].緯度Lat, data1[i].經度Lng);
+	                    	//加一個Marker到map中
+	                    	var image='img/toilet3.png';
+	                    	var marker = new google.maps.Marker({
+	                        	position: myLatlng,
+	                        	map: map,
+	                        	icon:image,
+	                        	html:"<ul><li>名稱 : "+type+"</li><li>地址 : "+area+"</li></ul>"
+	                    	});
+	                    	//點擊對話框事件    
+	                   		google.maps.event.addListener(marker, 'click', function(){ 
+	                  			infowindow.setContent(this.html);
+	                  			infowindow.open(map,this);
+	                   		});
+		                }//End if(area.match("楠梓區"))
+	            }//End for(i=0;i<data1.length;i++)
+	        }//End if(select1 !== "null")
+	    });//End .then(function()
 
 	//查詢事件
     $(".search").click(function(){
-        var data1;
+        var data2;
         $.when(
           	$.ajax({
             type:'GET',
@@ -13,7 +66,7 @@ function init(){
             async: false,
             dataType: 'json', 
             success:function(data){
-              data1=data; 
+              data2=data; 
       		  }
           	})
         ).then(function(){
@@ -21,43 +74,27 @@ function init(){
         var select2=[];
             $("[name=Type]:checkbox:checked").each(function(){
               select2.push($(this).val());
-              console.log(select2);
             });
         var select3=$("input[name=accessible]:checked").val();
-        var first = true;
         //對話框
         var infowindow = new google.maps.InfoWindow();
         var map;
         if(select1 !== "null"){
-        	for(i=0;i<data1.length;i++){
-	            if (first == true) {//第一次執行迴圈
-                    /*以哪個緯經度中心來產生地圖*/
-                    var latlng = new google.maps.LatLng(data1[i].緯度Lat, data1[i].經度Lng);
-                    var myOptions = {
-                        zoom: 14,
-                        center: latlng,
-                        mapTypeId: google.maps.MapTypeId.ROADMAP
-                    };
-                    /*產生地圖*/
-                    map = new google.maps.Map($("#map")[0], myOptions);
-
-                    first = false;
-                } //End if (first == true) 
+        	for(x=0;x<data2.length;x++){
 	            var str;
-	            var area=data1[i].address;
-	            var type=data1[i].name;
+	            var area=data2[x].address;
+	            var type=data2[x].name;
 	                if(area.match(select1)){
 	                	if(type.match(select3)){
 	                		for(j=0;j<select2.length;j++){
 		                		select=select2[j];
 		                		if(type.match(select)){
 		                			//建立緯經度座標
-                    				var myLatlng = new google.maps.LatLng(data1[i].緯度Lat, data1[i].經度Lng);
+                    				var myLatlng = new google.maps.LatLng(data2[x].緯度Lat, data2[x].經度Lng);
                     				//加一個Marker到map中
                     				var image='img/toilet3.png';
                     				var marker = new google.maps.Marker({
                         				position: myLatlng,
-                        				center:myLatlng,
                         				map: map,
                         				icon:image,
                         				html:"<ul><li>名稱 : "+type+"</li><li>地址 : "+area+"</li></ul>"
@@ -69,12 +106,11 @@ function init(){
                    					});
 		                		}else{
 		                			//建立緯經度座標
-                    				var myLatlng = new google.maps.LatLng(data1[i].緯度Lat, data1[i].經度Lng);
+                    				var myLatlng = new google.maps.LatLng(data2[x].緯度Lat, data2[x].經度Lng);
                     				//加一個Marker到map中
                     				var image='img/toilet3.png';
                     				var marker = new google.maps.Marker({
                         				position: myLatlng,
-                        				center:myLatlng,
                         				map: map,
                         				icon:image,
                         				html:"<ul><li>名稱 : "+type+"</li><li>地址 : "+area+"</li></ul>"
@@ -88,7 +124,7 @@ function init(){
 	                		}//End for(j=0;j<select2.length;j++)
 	                	}//End if(type.match(select3))
 	                }//End if(area.match(select1))
-            }//End for(i=0;i<data1.length;i++)
+            }//End for(x=0;x<data2.length;x++)
         }//End if(select1 !== "null")
         
         });//End .then(function()
